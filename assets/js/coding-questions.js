fd1eba992ba853ce3bfad386db30e2a97a62ff22
rohannegi <rohangovindnegi@yahoo.com>
2020-12-31 14:02:43 -0500

    initial html

---
+++ assets/js/coding-questions.js
@@ -0,0 +1,28 @@
+// coding-questions
+var codingquestions = [
+    {
+        title: "1. Which of the following is not a reserved word in JavaScript?",
+        choices: ["interface", "program", "throws", "short"],
+        answer: "program"
+    },
+    {
+        title: "2. What is the HTML tag under which one can write the JavaScript code?",
+        choices: ["<script>", "<javascript>", "<scripted>", " <js>"],
+        answer: "<script>"
+    },
+    {
+        title: "3. Which of the following is an advantage of using JavaScript?",
+        choices: ["Less server interaction", "Immediate feedback to the visitors", "Increased interactivity", "All of the above"],
+        answer: "All of the above"
+    },
+    {
+        title: "4. Which of the following is the correct syntax to print a page using JavaScript?",
+        choices: ["browser.print();", "navigator.print();", "document.print();", "window.print();"],
+        answer: "window.print();"
+    },
+    {
+        title: "5. Which of the following function of String object causes a string to be displayed as a superscript, as if it were in a <sup> tag?",
+        choices: ["small()", "strike()", "sub()", "sup()"],
+        answer: "sup()"
+    }
+]
