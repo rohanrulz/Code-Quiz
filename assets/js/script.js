6003fd5cd4491f6f06d325840137efc13f48da0b
rohannegi <rohangovindnegi@yahoo.com>
2020-12-31 16:16:54 -0500

    some js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,29 @@
 //variables
 
+var indexofcurrentquestion = 0;
+var randomtime = codingquestions.length * 15;
+var randomtimerId;
+
+var timerelatedEl = document.querySelector("#time-related");
+var startic = document.querySelector("#starticon");
+var quiztitlescreen = document.querySelector("quiz-title");
+var quizdisplay = document.querySelector("#quiz-questions");
+var random2El = document.querySelector("random-2");
+var optionsEl = document.querySelector("options");
+
+
+function beginQuiz() {
+
+  quiztitlescreen.setAttribute("class", "hide");
+
+  quizdisplay.setAttribute("class", "show");
+
+  randomtimerId = setInterval(startticking, 1000);
+
+  timerelatedEl.textcontent = randomtime;
+
+  displayQuestion(); 
+  
+}
+
+
